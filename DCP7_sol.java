eb00e78c7cbc0159f73c4c6ab229f6e7ba5e816a
akashkolte11 <56830049+akashkolte11@users.noreply.github.com>
2020-07-16 10:44:27 +0530

    Add files via upload

---
+++ DCP7_sol.java
@@ -0,0 +1,20 @@
+class MaximumSum { 
+	int FindMaxSum(int arr[], int n){ 
+		int incl = arr[0]; 
+		int excl = 0; 
+		int excl_new; 
+
+		for(int i = 1; i<n; i++){ 
+			excl_new = (incl > excl)?incl:excl; 
+			incl = excl + arr[i]; 
+			excl = excl_new; 
+		}
+    	return ((incl>excl)?incl:excl); 
+	}
+
+    public static void main(String[] args){ 
+		MaximumSum sum = new MaximumSum(); 
+		int arr[] = new int[]{5, 5, 10, 100, 10, 5}; 
+		System.out.println(sum.FindMaxSum(arr, arr.length)); 
+	} 
+}
